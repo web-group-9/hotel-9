cb93ef57e4af98a193f8ad9f9bf0ae5827cfc60a
陶冠丞 <t111590450@ntut.org.tw>
2024-01-05 14:51:16 +0800

    bootstraps added

--- backend/backend.py
+++ backend/backend.py
@@ -14,8 +14,60 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = os.getenv("SQLALCHEMY_TRACK_MODIF
 db = SQLAlchemy(app)
 
 @app.route("/")
-def hello_world():
-    return render_template("index.html", title=app.name)
+def index():
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/utilities-other")
+def utilities_other():
+    return render_template("utilities-other.html")
+
+@app.route("/utilities-color")
+def utilities_color():
+    return render_template("utilities-color.html")
+
+@app.route("/utilities-border")
+def utilities_border():
+    return render_template("utilities-border.html")
+
+@app.route("/utilities-animation")
+def utilities_animation():
+    return render_template("utilities-animation.html")
+
+@app.route("/tables")
+def tables():
+    return render_template("tables.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/charts")
+def charts():
+    return render_template("charts.html")
+
+@app.route("/cards")
+def cards():
+    return render_template("cards.html")
+
+@app.route("/buttons")
+def buttons():
+    return render_template("buttons.html")
+
+@app.route("/blank")
+def blank():
+    return render_template("blank.html")
+
+@app.route("/404")
+def error_404():
+    return render_template("404.html")
+
+@app.route("/forgot-password")
+def forgotpassword():
+    return render_template("forgot-password.html")
 
 if __name__ == "__main__":
     app.run(host=os.getenv("HOST"), port=os.getenv("HOST_PORT"), debug=os.getenv("DEBUG"))
--- frontend/frontend.py
+++ frontend/frontend.py
@@ -7,8 +7,20 @@ load_dotenv()
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template("index.html", title=app.name)
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/products")
+def products():
+    return render_template("products.html")
+
+@app.route("/store")
+def store():
+    return render_template("store.html")
 
 if __name__ == "__main__":
     app.run(host=os.getenv("HOST"), port=os.getenv("HOST_PORT"), debug=os.getenv("DEBUG"))
